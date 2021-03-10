5a973ea7c8acac2dabeb24e0d2cdcca116dff4d6
zavdev <igor.zaviriukha@onix-systems.com>
2021-03-10 13:07:28 +0200

    added postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,12 @@ module.exports = {
     rules: [
       {
         test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
